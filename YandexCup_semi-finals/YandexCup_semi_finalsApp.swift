9a181e5908f85dccd7b84c332aef2caa03962c12
Uvexer <125573358+Uvexer@users.noreply.github.com>
2024-10-28 18:56:00 +0300

    Initial Commit

---
+++ YandexCup_semi-finals/YandexCup_semi_finalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexCup_semi_finalsApp.swift
+//  YandexCup_semi-finals
+//
+//  Created by bodya on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct YandexCup_semi_finalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
