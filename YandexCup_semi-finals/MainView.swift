c60deb8ceacffd3d1f8a0a33226aa66938a9d079
Uvexer <125573358+Uvexer@users.noreply.github.com>
2024-11-04 09:49:59 +0300

    add colors ui

--- YandexCup_semi-finals/MainView.swift
+++ YandexCup_semi-finals/MainView.swift
@@ -16,7 +16,7 @@ struct MainView: View {
     @State private var isStratumViewPresented = false
     @State private var frames: [UIImage] = []
     @State private var currentFrameImage: UIImage? = nil
-    @State private var figures: [Figure] = [] 
+    @State private var figures: [Figure] = []
 
     var body: some View {
         VStack {
@@ -74,6 +74,23 @@ struct MainView: View {
                             .scaleEffect(activeImage == "figures" ? 1 : 0.8)
                             .opacity(activeImage == "figures" ? 1 : 0)
                             .animation(.easeInOut(duration: 0.5), value: activeImage)
+                        } else if activeImage == "eclipsik" {
+                            HStack(spacing: 20) {
+                                Image("drawer").onTapGesture { addEclipsik("drawer") }
+                                Image("circle1").onTapGesture { addEclipsik("circle1") }
+                                Image("circle2").onTapGesture { addEclipsik("circle2") }
+                                Image("circle3").onTapGesture { addEclipsik("circle3") }
+                                Image("circle4").onTapGesture { addEclipsik("circle4") }
+                            }
+                            .font(.title)
+                            .padding()
+                            .background(BlurView(style: .systemMaterial))
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .offset(x: -30, y: -50)
+                            .scaleEffect(activeImage == "eclipsik" ? 1 : 0.8)
+                            .opacity(activeImage == "eclipsik" ? 1 : 0)
+                            .animation(.easeInOut(duration: 0.5), value: activeImage)
                         }
                     },
                     alignment: .bottomTrailing
@@ -91,7 +108,12 @@ struct MainView: View {
         let newFigure = Figure(type: type, position: CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2))
         figures.append(newFigure)
     }
+
+    private func addEclipsik(_ name: String) {
+        
+    }
 }
+
 
 struct BlurView: UIViewRepresentable {
     var style: UIBlurEffect.Style
